5981efcd481acf7c82b8961363936b3532ccf41d
oladee <deemajor230600@gmail.com>
2022-07-03 18:43:03 +0100

    Insomia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - ENtry Point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int z;
+
+	for (z = 'a'; z <= 'z'; z++)
+	{
+		putchar(z);
+		putchar('\n');
+	} return (0);
+}
