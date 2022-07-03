d26ee26427b4b9600adfcefbc0dd5844ebba392b
oladee <deemajor230600@gmail.com>
2022-07-03 19:07:14 +0100

    AlphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * putchar - prints string out
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
