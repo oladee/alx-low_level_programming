e52532d98a01aa917f2a46705059e15cfda311cf
oladee <deemajor230600@gmail.com>
2022-07-06 22:16:21 +0100

    9 it is

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,13 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			k = i * j;
 			_putchar(k);
-			if (i < 9)
+			if (i < '9')
 			{
 				_putchar(',');
 				_putchar(' ');
