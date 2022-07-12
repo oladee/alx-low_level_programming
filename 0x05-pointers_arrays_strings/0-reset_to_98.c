fb24c866fa96e4f622d7b81bfed1781059fb2a9e
oladee <deemajor230600@gmail.com>
2022-07-13 00:56:27 +0100

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,10 @@
 /**
  * reset_to_98 - takes a pointer to an int
  * as parameter and updates the value it points to to 98
+ *
+ * @n: points to a data type of integer value
  **/
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
