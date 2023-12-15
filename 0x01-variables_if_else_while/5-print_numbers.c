a492c640c58cde096ab913b85389fb77e0553044
oladee <deemajor230600@gmail.com>
2023-12-16 00:34:26 +0100

    prints adecimal numbers form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of
+ *          base 10 starting from 0, followed by a new line
+ * Return: Always 0
+*/
+
+int main(void)
+{
+int dee;
+for (dee = 0; dee < 10; dee++)
+{
+printf(dee);
+}
+}
