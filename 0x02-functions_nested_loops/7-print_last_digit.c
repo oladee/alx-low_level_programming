d9c4188382935366d331092529644f82fccb34b7
oladee <deemajor230600@gmail.com>
2023-12-20 00:17:35 +0100

    script returns the last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @c: value to be evaluated
+ * Return: Always last digit of the value
+*/
+int print_last_digit(int c)
+{
+int d;
+d = c % 10;
+return (d);
+}
