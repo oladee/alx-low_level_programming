5b509863375e7bfd4833d39b13ac0132176c28f3
oladee <deemajor230600@gmail.com>
2023-12-20 01:39:13 +0100

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,15 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar((i * j) + '0');
+int k;
+k = i * j;
+_putchar(k + '0');
 if (j == 9)
 {
 continue;
 }
 _putchar(',');
+_putchar(' ');
 _putchar(' ');
 }
 _putchar('\n');
