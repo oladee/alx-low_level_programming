e91948625e71e44f6320379e9286d3275cc20090
oladee <deemajor230600@gmail.com>
2023-12-22 02:07:13 +0100

    prints diagonal line n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line in the terminal.
+ * @n: number of dashes to be drawn
+ * Return: Always nothing
+*/
+
+void print_diagonal(int n)
+{
+int b;
+if (n > 0)
+{
+for (b = 0; b < n; b++)
+{
+_putchar('\\');
+}
+}
+_putchar('\n');
+return;
+}
