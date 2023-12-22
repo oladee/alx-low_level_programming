5e55725d49906a5a10d95e81d09633d547c2c12f
oladee <deemajor230600@gmail.com>
2023-12-22 02:28:52 +0100

    prints diagonal line n times

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,21 @@
 
 void print_diagonal(int n)
 {
-int b;
+int b, c;
 if (n > 0)
 {
 for (b = 0; b < n; b++)
 {
+for (c = 0; c < b; c++)
+{
 _putchar(' ');
 }
-_putchar('\\');
-}
+_putchar(92);
 _putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
