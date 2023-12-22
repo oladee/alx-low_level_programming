c5cb3d4eb40f13b44243b87e83982be3a4bfe362
oladee <deemajor230600@gmail.com>
2023-12-22 02:36:34 +0100

    prints diagonal line n times

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,19 @@
 void print_square(int size)
 {
 int x, y;
+if (size > 0)
+{
 for (x = 0; x < size; x++)
 {
 for (y = 0; y < size; y++)
 {
 _putchar('#');
 }
+_putchar('\n');
+}
+}
+else
+{
 _putchar('\n');
 }
 return;
