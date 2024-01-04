504e93aa6771aa5ca9e3ada9fb0fea8cacc85558
Oladee <deemajor230600@gmail.com>
2024-01-04 01:25:10 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new
+ *          line, to stdout.
+ * @str: pointer to string
+ * Return: Always Nothing
+*/
+void _puts(char *str)
+{
+char *d;
+d = str;
+while(*d)
+{
+_putchar(*d);
+}
+}
