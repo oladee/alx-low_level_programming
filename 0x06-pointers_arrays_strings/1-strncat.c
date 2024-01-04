ffff7d940cf716fa9e0005232aa7c2e202e954a1
Oladee <deemajor230600@gmail.com>
2024-01-04 23:35:19 +0100

    str cat2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,22 @@
 #include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ * @n: number of byte to be added
+ * Return: Always the pointer to the string
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-int destLen, index, srcLen, index2;
+int destLen, index;
 destLen = 0;
-srcLen = 0;
 index = 0;
-index2 = 0;
 while (dest[index++])
 {
 destLen++;
 }
-for(index = 0; index < n && src[index]; index++)
+for (index = 0; index < n && src[index]; index++)
 {
 dest[destLen++] = src[index];
 }
