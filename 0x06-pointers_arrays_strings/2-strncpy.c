64780d9fd314559942feb990fc834b48328c96c4
Oladee <deemajor230600@gmail.com>
2024-01-04 23:47:57 +0100

    str cpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int index;
-for(index = 0; index < n && src[index]; index++)
+for (index = 0; index < n && src[index]; index++)
 {
 dest[index] = src[index];
 }
